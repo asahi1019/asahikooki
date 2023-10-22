21c6a7e18fbd5ad993e977ab46540fae99a0bcd3
asahi1019 <131091854+asahi1019@users.noreply.github.com>
2023-10-23 00:16:33 +0900

    Update ex6-6.js

--- ex6-6.js
+++ ex6-6.js
@@ -2,7 +2,7 @@ var who66 = 'world';
 var who66 = '太郎が来た';
 var who66 = '花子が来た';
 function sayhello66() { 
-  alert( 'who66' + '!');
+  alert( who66 + '!');
 }
 function taro66() { 
   who66 = '太郎が来た';
