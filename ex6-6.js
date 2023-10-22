df79a9b8949f46358028514c23e59dc373f817c1
asahi1019 <131091854+asahi1019@users.noreply.github.com>
2023-10-22 22:20:44 +0900

    Delete ex6-6.js

--- ex6-6.js
+++
@@ -1,5 +0,0 @@
-var who66 = 'world';
-var who67 = '太郎が来た';
-var who68 = '花子が来た';
-function sayhello65(){ alert(Hello, '+who65 + '!');}
-function taro65(){ who65 ='太郎';} function hanako65(){ who65 ='花子';}
