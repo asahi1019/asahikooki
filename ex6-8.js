2bad1d16c206694253526c0e9fa00322ccca1c26
asahi1019 <131091854+asahi1019@users.noreply.github.com>
2023-10-31 23:56:02 +0900

    Update ex6-8.js

--- ex6-8.js
+++ ex6-8.js
@@ -1,10 +1,10 @@
 function sayhello68(){
-  alert('Hello,' + document.getElementByld('who68').innerHTML + '!');
+  alert('Hello,' + document.getElementById('who68').innerHTML + '!');
 }
 function taro68(){
-  document.getElementByld('who68').innerHTML='太郎';
+  document.getElementById('who68').innerHTML='太郎';
 }
 function hanako68(){
-  document.getElementByld('who68').innerHTML='花子';
+  document.getElementById('who68').innerHTML='花子';
 }
 function gazo688(){ document.getElementById('who688').innerHTML='<img src="./image/dhiaruga.jpg" alt="オン">';}
