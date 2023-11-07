15180b6318a71f62dfd4a3c72ed0703b20f558b5
asahi1019 <131091854+asahi1019@users.noreply.github.com>
2023-11-07 10:39:01 +0900

    Update ex6-10.js

--- ex6-10.js
+++ ex6-10.js
@@ -1,6 +1,6 @@
 function imgchange1(){
   document.getElementById('logo').src=
     './image/josai1.png';}
-  function imgchange1(){
+  function imgchange2(){
   document.getElementById('logo').src=
     './image/josai2.png';}
