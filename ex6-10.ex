377271e6b043c1b0a3c3ffaec9671f4cb50ad9e2
asahi1019 <131091854+asahi1019@users.noreply.github.com>
2023-11-07 10:19:12 +0900

    Delete ex6-10.ex

--- ex6-10.ex
+++
@@ -1,6 +0,0 @@
-function imgchange1(){
-  document.getElementByld('logo').src=
-    './image/josai1.gif';}
-  function imgchange1(){
-  document.getElementByld('logo').src=
-    './image/josai2.gif';}
