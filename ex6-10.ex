adc39c0757638c40b3b51bb12949648d6f174b1d
asahi1019 <131091854+asahi1019@users.noreply.github.com>
2023-11-07 10:16:24 +0900

    Create ex6-10.ex

---
+++ ex6-10.ex
@@ -0,0 +1,6 @@
+function imgchange1(){
+  document.getElementByld('logo').src=
+    './image/josai1.gif';}
+  function imgchange1(){
+  document.getElementByld('logo').src=
+    './image/josai2.gif';}
