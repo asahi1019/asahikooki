51e04381d5b0a09f5381ad7ad550ac21f365932c
asahi1019 <131091854+asahi1019@users.noreply.github.com>
2023-11-14 10:24:00 +0900

    Update ex6-10-2.js

--- ex6-10-2.js
+++ ex6-10-2.js
@@ -1,1 +1,6 @@
-
+function imgchange3(){
+  document.getElementById('logo').src=
+    './image/.png';}
+  function imgchange4(){
+  document.getElementById('logo').src=
+    './image/bird_pink_robin.jpg';}
